a268135c2406e2448a4582fdc6e36fa0eb971085
yangdepp <965093975@qq.com>
2019-01-31 10:16:32 +0800

    hot-module-replacement

--- client/app.js
+++ client/app.js
@@ -1,5 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App.jsx'
+import { AppContainer } from 'react-hot-loader'
 
-ReactDOM.hydrate(<App />, document.getElementById('root'))
+import App from './App.jsx'
+// ReactDOM.hydrate(<App />, document.getElementById('root'))
+
+const root = document.getElementById('root')
+const render = Component =>{
+  ReactDOM.hydrate(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    root
+  )
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./App.jsx', () => {
+    // require方式，commonjs规范要加上default
+    const NextApp = require('./App.jsx').default
+    // ReactDOM.hydrate(<NextApp />, document.getElementById('root'))
+    render(NextApp)
+  })
+}
