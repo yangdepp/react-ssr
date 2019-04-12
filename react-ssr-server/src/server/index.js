cf65a9fa2bc35f81cc77d9bb718401a83e109ed1
yangdepu <yangdepu@zdsoft.cn>
2019-04-12 17:55:54 +0800

    重构代码

--- react-ssr-server/src/server/index.js
+++ react-ssr-server/src/server/index.js
@@ -1,12 +1,43 @@
-const express = require('express');
+import express from 'express';
+import { matchRoutes } from 'react-router-config';
 import { render } from './utils';
+import routes from '../Routes';
+import getStore from '../store';
 
 const app = express();
 //  express返回一个静态文件
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-  render(req, res);
+  const store = getStore();
+  /*   如果在这里，可以拿到异步数据，并填充到store中，这时store就有数据了
+  服务端渲染时，store里填充什么数据，需要根据当前用户请求地址、路由判断
+  如： 访问 / 路径，就拿home组件的异步数据 ; 访问 /login 路径，拿login组件的异步数据 */
+
+  /*   根据路由的路径，往store里面加数据
+  let matchedRoutes = [];
+  请求的路径和某一项的路径能匹配上,就把路由item push进matchRoutes数组中
+  此方法不能匹配多级路由,用下面的matchedRoutes
+  routes.some(route => {
+    const match = matchPath(req.path, route);
+    if (match) {
+      matchedRoutes.push(route);
+    }
+  }); */
+
+  // matchedRoutes里面是所有匹配到的组件
+  const matchedRoutes = matchRoutes(routes, req.path);
+
+  let promises = [];
+  matchedRoutes.forEach(item => {
+    if (item.route.loadData) {
+      promises.push(item.route.loadData(store));
+    }
+  });
+
+  Promise.all(promises).then(() => {
+    res.send(render(store, routes, req));
+  });
 });
 
 const server = app.listen(3000);
--- react-ssr-server/src/server/utils.js
+++ react-ssr-server/src/server/utils.js
@@ -1,51 +1,22 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter, Route } from 'react-router-dom';
-import { matchRoutes } from 'react-router-config';
-import routes from '../Routes';
 import { Provider } from 'react-redux';
-import getStore from '../store';
 
-export const render = (req, res) => {
-  const store = getStore();
-  /*   如果在这里，可以拿到异步数据，并填充到store中，这时store就有数据了
-  服务端渲染时，store里填充什么数据，需要根据当前用户请求地址、路由判断
-  如： 访问 / 路径，就拿home组件的异步数据 ; 访问 /login 路径，拿login组件的异步数据 */
+export const render = (store, routes, req) => {
+  const content = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.path} context={{}}>
+        <div>
+          {routes.map(route => (
+            <Route {...route} />
+          ))}
+        </div>
+      </StaticRouter>
+    </Provider>,
+  );
 
-  /*   根据路由的路径，往store里面加数据
-  let matchedRoutes = [];
-  请求的路径和某一项的路径能匹配上,就把路由item push进matchRoutes数组中
-  此方法不能匹配多级路由
-  routes.some(route => {
-    const match = matchPath(req.path, route);
-    if (match) {
-      matchedRoutes.push(route);
-    }
-  }); */
-
-  // matchedRoutes里面是所有匹配到的组件
-  const matchedRoutes = matchRoutes(routes, req.path);
-  let promises = [];
-  matchedRoutes.forEach(item => {
-    if (item.route.loadData) {
-      promises.push(item.route.loadData(store));
-    }
-  });
-
-  Promise.all(promises).then(() => {
-    const content = renderToString(
-      <Provider store={store}>
-        <StaticRouter location={req.path} context={{}}>
-          <div>
-            {routes.map(route => (
-              <Route {...route} />
-            ))}
-          </div>
-        </StaticRouter>
-      </Provider>,
-    );
-
-    res.send(`
+  return `
     <html>
       <head>
         <title>react-ssr</title>
@@ -56,6 +27,5 @@ export const render = (req, res) => {
       <script src="index.js"></script>
       </body>
     </html>
-  `);
-  });
+  `;
 };
